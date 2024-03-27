f01604cec45eeb0511be59ea4afb103c24889be1
Yuvraj Singh <yuwe18b@gmail.com>
2024-03-27 22:41:19 +0530

    feat: add output in comments

--- blocks-of-code.java
+++ blocks-of-code.java
@@ -14,3 +14,23 @@ class BlockTest
     } 
 }
 
+// output:  This is x: 0
+//          This is y: 20
+//          This is x: 1
+//          This is y: 18
+//          This is x: 2    
+//          This is y: 16
+//          This is x: 3
+//          This is y: 14
+//          This is x: 4
+//          This is y: 12
+//          This is x: 5
+//          This is y: 10
+//          This is x: 6
+//          This is y: 8
+//          This is x: 7
+//          This is y: 6
+//          This is x: 8
+//          This is y: 4
+//          This is x: 9
+//          This is y: 2
