87174bc4adf0a28f0ce58ab28db05de27cb64734
Yuvraj Singh <yuwe18b@gmail.com>
2024-03-29 22:41:41 +0530

    feat: add new lines of code

--- datatypes,variables and array/primitive types/char2.java
+++ datatypes,variables and array/primitive types/char2.java
@@ -12,3 +12,7 @@ class CharDemo2
     } 
 }
 
+// output:  ch1 contains X
+//           ch1 is now Y
+
+// In this program, a char variable is declared and initialized with the character X. The value of ch1 is then displayed. Next, ch1 is incremented. When the program is run, the output is ch1 contains X ch1 is now Y. This program demonstrates that char variables can be used in arithmetic expressions. In this case, ch1 is incremented by one. The result is the character Y, which is displayed by the second println( ) statement.
