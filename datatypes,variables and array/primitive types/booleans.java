d85320130d516d7b563d15b601ce1b692df061dc
Yuvraj Singh <yuwe18b@gmail.com>
2024-03-29 23:09:21 +0530

    feat: add comment for output and logic

--- datatypes,variables and array/primitive types/booleans.java
+++ datatypes,variables and array/primitive types/booleans.java
@@ -24,3 +24,10 @@ class BoolTest
     } 
 }
 
+// output:  b is false
+//          b is true
+//          This is executed.
+//          10 > 9 is true
+
+// In this program, a boolean variable called b is declared and initialized to false. The value of b is then displayed. Next, b is set to true and displayed. The value of b is then used to control an if statement. The if statement is executed because b is true. Next, b is set to false and used to control another if statement. This time, the if statement is not executed because b is false. Finally, the outcome of the relational expression 10 > 9 is displayed. This expression evaluates to true, so the output is 10 > 9 is true.
+
