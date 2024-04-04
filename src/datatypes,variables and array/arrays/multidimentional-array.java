0570c747b0f6c27c3bab59535b0da3cd3c7b002f
Yuvraj Singh <yuwe18b@gmail.com>
2024-04-05 00:00:50 +0530

    feat: add 2 d array code

---
+++ src/datatypes,variables and array/arrays/multidimentional-array.java
@@ -0,0 +1,30 @@
+// Demonstrate a two-dimensional array. 
+
+class TwoDArray 
+{ 
+    public static void main(String[] args) 
+    { 
+        int[][] twoD= new int[4][5]; 
+        int i, j, k = 0; 
+        
+        for(i=0; i<4; i++) 
+        
+        for(j=0; j<5; j++) 
+        { 
+            twoD[i][j] = k; 
+            k++; 
+        } 
+        
+        for(i=0; i<4; i++) 
+        { 
+            for(j=0; j<5; j++) 
+            System.out.print(twoD[i][j] + " "); 
+            
+            System.out.println(); 
+        } 
+    } 
+}
+
+// output: 0 1 2 3 4
+//         5 6 7 8 9
+
