f068df71745d3f6f24eb577d0d4651232d28a3b2
Yuvraj Singh <yuwe18b@gmail.com>
2024-04-05 23:27:48 +0530

    feat: add class a code and import file

---
+++ src/class and objects/contructor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class A
+{
+	private int x=46, y=53;
+	void sum()
+	{
+		int s=x+y;
+		System.out.println("The sum is "+s);
+	}
+	/*A(int i, int j)
+	{
+		x=i;
+		y=j;
+		System.out.println("Constructor Called");
+	}*/
+}
