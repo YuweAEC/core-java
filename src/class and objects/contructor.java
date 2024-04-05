ed0d821a4602881bd4621e6fc87570562c174eff
Yuvraj Singh <yuwe18b@gmail.com>
2024-04-05 23:30:13 +0530

    feat: add contructordemo class and invoke file

--- src/class and objects/contructor.java
+++ src/class and objects/contructor.java
@@ -15,3 +15,22 @@ class A
 		System.out.println("Constructor Called");
 	}*/
 }
+
+class ConstructorDemo
+{
+	public static void main(String s[])
+	{
+		/*Scanner p=new Scanner(System.in);
+		System.out.print("Enter values for the 1st object ");
+		int m=p.nextInt();
+		int n=p.nextInt();*/
+		A obj1=new A();
+		//A obj2=new A(6,24);
+		
+		obj1.sum();
+		//obj2.sum();
+	}
+}
+
+// output: The sum is 99
+//         The sum is 30
