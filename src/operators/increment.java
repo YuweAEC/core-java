e95c8ca381016a4ae7b3fdb7882213afa2335822
Yuvraj Singh <yuwe18b@gmail.com>
2024-04-08 00:46:02 +0530

    feat: add code of increment

---
+++ src/operators/increment.java
@@ -0,0 +1,24 @@
+// Demonstrate ++. 
+class IncDec 
+{ 
+    public static void main(String[] args) 
+    { 
+        int a = 1; 
+        int b = 2; 
+        int c; 
+        int d; 
+        c = ++b; 
+        d = a++; 
+        c++; 
+        System.out.println("a = " + a); 
+        System.out.println("b = " + b); 
+        System.out.println("c = " + c); 
+        System.out.println("d = " + d); 
+    } 
+}
+
+// output: a = 2
+//         b = 3
+//         c = 4
+//         d = 1
+
