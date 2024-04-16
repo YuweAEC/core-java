f833a884c6c4db14393b16ffa043791b95809bc0
Yuvraj Singh <yuwe18b@gmail.com>
2024-04-16 23:07:15 +0530

    refactor: add comma code for for loop control

--- src/control statement/iteration statements/for-control.java
+++ src/control statement/iteration statements/for-control.java
@@ -25,3 +25,19 @@ class Sample
         } 
     } 
 }
+
+// Using the comma. 
+class Comma 
+{ 
+    public static void main(String[] args) 
+    { 
+        int a, b; 
+        
+        for(a=1, b=4; a<b; a++, b--) 
+        { 
+            System.out.println("a = " + a); 
+            System.out.println("b = " + b); 
+        } 
+    } 
+}
+
