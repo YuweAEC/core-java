e1dffdd6bb9c8586d279eade61fccb886abb9856
Yuvraj Singh <yuwe18b@gmail.com>
2024-04-24 23:50:03 +0530

    feat: add new lines of code for the objects passing parameter

--- core_java/methods_&_classes/objects_parameters.java
+++ core_java/methods_&_classes/objects_parameters.java
@@ -17,3 +17,16 @@ class Test
     } 
 }
 
+class PassObject 
+{ 
+    public static void main(String[] args) 
+    { 
+        Test ob1 = new Test(100, 22); 
+        Test ob2 = new Test(100, 22); 
+        Test ob3 = new Test(-1, -1); 
+        
+        System.out.println("ob1 == ob2: " + ob1.equalTo(ob2)); 
+        System.out.println("ob1 == ob3: " + ob1.equalTo(ob3)); 
+    } 
+}
+
