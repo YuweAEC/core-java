f9c1ba692251cf49fac81cf35b9c19d4f27037ef
Yuvraj Singh <yuwe18b@gmail.com>
2024-04-25 23:20:52 +0530

    feat: add code for returning objects

---
+++ core_java/methods_&_classes/returning_objects.java
@@ -0,0 +1,18 @@
+// Returning an object. 
+
+class Test 
+{ 
+    int a; 
+    
+    Test(int i) 
+    { 
+        a = i;
+    } 
+    
+    Test incrByTen() 
+    { 
+        Test temp = new Test(a+10); 
+        return temp; 
+    } 
+}
+
