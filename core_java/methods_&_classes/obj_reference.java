47daa4faef2109131802aa494ab5d0cae8d70dd3
Yuvraj Singh <yuwe18b@gmail.com>
2024-04-25 22:23:35 +0530

    feat: ad new lines of code for object reference

--- core_java/methods_&_classes/obj_reference.java
+++ core_java/methods_&_classes/obj_reference.java
@@ -2,14 +2,19 @@
 class Test 
 { 
     int a, b; 
-    Test(int i, int j) { 
-    a = i; 
-    b = j; 
+    
+    Test(int i, int j) 
+    { 
+        a = i; 
+        b = j; 
     } 
+    
     // pass an object 
-    void meth(Test o) { 
-    o.a *= 2; 
-    o.b /= 2; 
+    
+    void meth(Test o) 
+    { 
+        o.a *= 2; 
+        o.b /= 2; 
     } 
 } 
 
@@ -17,12 +22,14 @@ class PassObjRef
 { 
     public static void main(String[] args) 
     { 
-    Test ob = new Test(15, 20); 
-    System.out.println("ob.a and ob.b before call: " + 
-    ob.a + " " + ob.b); 
-    ob.meth(ob); 
-    System.out.println("ob.a and ob.b after call: " + 
-    ob.a + " " + ob.b); 
+        Test ob = new Test(15, 20); 
+        System.out.println("ob.a and ob.b before call: " + 
+        
+        ob.a + " " + ob.b); 
+        ob.meth(ob); 
+        
+        System.out.println("ob.a and ob.b after call: " + 
+        ob.a + " " + ob.b); 
     } 
 }   
 
