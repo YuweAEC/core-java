a2cca290063bdbeff36cffa027be42da591887dd
Yuvraj Singh <yuwe18b@gmail.com>
2024-05-11 23:10:23 +0530

    refactor: add new class

--- core_java/inheritance/access_inheritance.java
+++ core_java/inheritance/access_inheritance.java
@@ -14,3 +14,20 @@ class A {
         j = y; 
     } 
 } 
+
+// A's j is not accessible here. 
+class B extends A { 
+    int total; 
+    void sum() { 
+        total = i + j; // ERROR, j is not accessible here 
+    } 
+} 
+
+class Access { 
+    public static void main(String[] args) { 
+        B subOb = new B(); 
+        subOb.setij(10, 12); 
+        subOb.sum(); 
+        System.out.println("Total is " + subOb.total); 
+    } 
+}
